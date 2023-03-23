dacbc1ab4e4063dddace766b360e742f420d6e85
Matvey Babikov <113699334+Mavelis@users.noreply.github.com>
2023-03-23 09:15:10 +0300

    Add files via upload

---
+++ echoserver/mytcpserver.cpp
@@ -0,0 +1,62 @@
+#include "mytcpserver.h"
+#include <QDebug>
+#include <QCoreApplication>
+
+QList<QTcpSocket*> clients;
+
+MyTcpServer::~MyTcpServer()
+{
+    int i = 0;
+    while(i < mTcpSocket.length())
+        mTcpSocket[i]->close();
+    mTcpServer->close();
+    server_status=0;
+}
+
+MyTcpServer::MyTcpServer(QObject *parent) : QObject(parent){
+    mTcpServer = new QTcpServer(this);
+    connect(mTcpServer, &QTcpServer::newConnection,
+            this, &MyTcpServer::slotNewConnection);
+
+    if(!mTcpServer->listen(QHostAddress::Any, 33333)){
+        qDebug() << "server is not started";
+    } else {
+        server_status=1;
+        qDebug() << "server is started";
+    }
+}
+
+void MyTcpServer::slotNewConnection(){
+
+    if(server_status==1){
+        QTcpSocket* cTcpSocket;
+        cTcpSocket = mTcpServer->nextPendingConnection();
+        cTcpSocket->write("Привет!!! Введите запрос!\r\n");
+        connect(cTcpSocket, &QTcpSocket::readyRead, this,&MyTcpServer::slotServerRead);
+        connect(cTcpSocket,&QTcpSocket::disconnected, this,&MyTcpServer::slotClientDisconnected);
+        mTcpSocket.push_back(cTcpSocket);
+    }
+
+}
+
+void MyTcpServer::slotServerRead(){
+    QByteArray array;
+    QTcpSocket* cTcpSocket = (QTcpSocket*)sender();
+    while(cTcpSocket->bytesAvailable()>0)
+    {
+        array.append(cTcpSocket->readAll());
+    }
+    if(array.right(1) == "\n")
+    {
+        cTcpSocket->write(array);
+    }
+    if(server_status==0){ //если сервер выключен
+            cTcpSocket = mTcpServer->nextPendingConnection();  //тому сокету, который есть у объекта присваем
+            cTcpSocket->write("Server sleep.");
+        }
+}
+
+void MyTcpServer::slotClientDisconnected(){
+    QTcpSocket* cTcpSocket = (QTcpSocket*)sender();
+    cTcpSocket->close();
+}
