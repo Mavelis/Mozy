64c1123bf7b835d470126f24fc65be14bf41f043
Matvey Babikov <113699334+Mavelis@users.noreply.github.com>
2023-06-19 15:52:43 +0300

    Add files via upload

---
+++ client/doxygen/html/search/all_5.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['taskform_0',['TaskForm',['../class_task_form.html',1,'']]],
+  ['teacherwindow_1',['TeacherWindow',['../class_teacher_window.html',1,'']]]
+];
---
+++ client/doxygen/html/search/classes_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['authwindow_0',['AuthWindow',['../class_auth_window.html',1,'']]]
+];
